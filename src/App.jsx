50ff018c570708fc0d5c168bc6eed819ee6de9e1
Leonardo Brum <leonardobrum.dev@gmail.com>
2024-09-05 17:36:34 -0300

    hidden photos section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Section1 from "./components/Sections/Section1";
 import Section2 from "./components/Sections/Section2";
 import Section3 from "./components/Sections/Section3";
-import Section4 from "./components/Sections/Section4";
+// import Section4 from "./components/Sections/Section4";
 import Section5 from "./components/Sections/Section5";
 import Section6 from "./components/Sections/Section6";
 
@@ -20,7 +20,7 @@ function App() {
 
         <Section3 id="section-3" />
 
-        <Section4 id="section-4" />
+        {/* <Section4 id="section-4" /> */}
 
         <Section5 id="section-5" />
 
